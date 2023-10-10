5c34dcb3913b9dcf4d7dfdc9739d1826c2ceb898
Amir6590hossein <amirhosseinamjadian1382@gmail.com>
2023-10-10 18:02:44 +0330

    complete with async await

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,47 +1,35 @@
 const Todo = require("../model/todo");
-const todoUtils = require("./../utils/todos");
 
-exports.addTodo = (req, res) => {
+exports.addTodo = async (req, res) => {
   if (!req.body.todo) return res.redirect("/");
-  Todo.create({ text: req.body.todo })
-    .then((result) => {
-      console.log(result);
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 
-  // const todo = new Todo(todoUtils.generateRandomId(), req.body.todo);
-  // todo.save((err) => {
-  //   if (!err) res.redirect("/");
-  //   else console.log(err);
-  // });
+  try {
+    await Todo.create({ text: req.body.todo });
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-exports.deleteTodo = (req, res) => {
-  // Todo.deleteTodo(req.params.id, (err) => {
-  //   if (!err) res.redirect("/");
-  //   else console.log(err);
-  // });
-  Todo.destroy({ where: { id: req.params.id } })
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.log(err));
+exports.deleteTodo = async (req, res) => {
+  // Todo.destroy({ where: { id: req.params.id } })
+  //     .then(() => res.redirect("/"))
+  //     .catch((err) => console.log(err));
+  try {
+    await Todo.destroy({ where: { id: req.params.id } });
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-exports.completeTodo = (req, res) => {
-  // Todo.setTodoToComplete(req.params.id, (err) => {
-  //   if (!err) res.redirect("/");
-  //   else console.log(err);
-  // });
-
-  Todo.findByPk(req.params.id)
-    .then((todo) => {
-      todo.completed = true;
-      return todo.save();
-    })
-    .then(res.redirect("/"))
-    .catch((err) => console.log(err));
+exports.completeTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findByPk(req.params.id);
+    todo.completed = true;
+    await todo.save();
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 };
--- controllers/todos.js
+++ controllers/todos.js
@@ -1,26 +1,13 @@
 const Todo = require("../model/todo");
 
-exports.getIndex = (req, res) => {
-  // todoUtils.getCompletedTodos((completedTodos) => {
-  //   todoUtils.getRemainingTodos((remainingTodos) => {
-  //     Todo.fetchAll((todos) => {
-  //       res.render("index", {
-  //         pageTitle: "کارهای روزمره",
-  //         todos,
-  //         remainingTodos,
-  //         completedTodos,
-  //       });
-  //     });
-  //   });
-  // });
-  Todo.count({ where: { completed: true } }).then((completedTodos) => {
-    Todo.findAll().then((todos) => {
-      res.render("index", {
-        pageTitle: "کارهای روزمره",
-        todos,
-        completedTodos,
-        remainingTodos: todos.length - completedTodos,
-      });
-    });
+exports.getIndex = async (req, res) => {
+  const completedTodos = await Todo.count({ where: { completed: true } });
+  const todos = await Todo.findAll();
+
+  res.render("index", {
+    pageTitle: "کارهای روزمره",
+    todos,
+    completedTodos,
+    remainingTodos: todos.length - completedTodos,
   });
 };
